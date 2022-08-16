df1029105876ed46d182b2e02e41985451de3134
martynasd123 <dargis.martynas@gmail.com>
2022-08-16 12:14:28 +0300

    Change plausible script url

--- src/Analytics.js
+++ src/Analytics.js
@@ -3,7 +3,7 @@ const initPlausible = () => {
     const plausible = document.createElement('script');
     plausible.async = true;
     plausible.type = 'text/javascript';
-    plausible.src = "//plausible.io/js/plausible.js";
+    plausible.src = "//analitika.zodziuks.lt/js/plausible.js";
     plausible.dataset.domain = "zodziuks.lt";
     const head = document.getElementsByTagName('head')[0];
     head.appendChild(plausible);
